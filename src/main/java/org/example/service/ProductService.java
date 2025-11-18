a7af367902a17c30429d984e53664c482b1172a8
Ayezah Maryam <ayeza.work43@gmail.com>
2025-11-18 16:33:16 +0500

    minor changes

--- src/main/java/org/example/service/ProductService.java
+++ src/main/java/org/example/service/ProductService.java
@@ -4,23 +4,17 @@ import org.example.api.model.Product;
 import org.example.entity.ProductEntity;
 import org.example.repository.ProductRepository;
 import org.modelmapper.ModelMapper;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.awt.print.Pageable;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
 
-    private final ProductRepository productRepository;
-    private final ModelMapper modelMapper;
-
-    public ProductService(ProductRepository productRepository, ModelMapper modelMapper) {
-        this.productRepository = productRepository;
-        this.modelMapper = modelMapper;
-    }
+    @Autowired private ProductRepository productRepository;
+    @Autowired private ModelMapper modelMapper;
 
     public List<Product> getAllProducts() {
         List<ProductEntity> products = productRepository.findByIsDeletedFalse();
---
+++ src/main/resources/db/changelog/sql/002-added-more-products.sql
@@ -0,0 +1,4 @@
+INSERT INTO products (name, description, price, stock_qty)
+VALUES
+('Wireless Mouse', 'Ergonomic wireless mouse with USB receiver', 1499.99, 50),
+('Laptop Stand', 'Adjustable aluminum laptop stand', 2999.99, 40);
